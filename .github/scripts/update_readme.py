9bb014f505b0946e3fcbd2f4448f791fdfe9996b
Dimitris Pantazatos <dim.pantazatos@gmail.com>
2023-10-10 15:48:11 +0300

    Update update_readme.py

--- .github/scripts/update_readme.py
+++ .github/scripts/update_readme.py
@@ -6,13 +6,20 @@ from tabulate import tabulate
 with open('data.json', 'r') as f:
     data = json.load(f)
 
+print("Data Loaded:", data)  # Debug print
+
 # Convert to DataFrame
 df = pd.DataFrame(data[1:], columns=data[0])
 
+print("DataFrame Created:", df.head())  # Debug print
+
 # Convert to markdown table
 md_table = tabulate(df, headers='keys', tablefmt='pipe', showindex=False)
+
+print("Markdown Table:", md_table)  # Debug print
 
 # Update README.md
 with open('README.md', 'w') as f:
     f.write("# My Prompt Library\n\n")
     f.write(md_table)
+
