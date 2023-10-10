e40ab911a2a4f4ea57dd8ca6bd9da1c84efd5ae6
Dimitris Pantazatos <dim.pantazatos@gmail.com>
2023-10-10 16:13:24 +0300

    Update update_markdown.py

--- .github/scripts/update_markdown.py
+++ .github/scripts/update_markdown.py
@@ -1,25 +1,33 @@
-import json
 import pandas as pd
-from tabulate import tabulate
+import json
 
-# Load data
-with open('data.json', 'r') as f:
-    data = json.load(f)
+# Load JSON data
+with open('data.json', 'r') as file:
+    data = json.load(file)
 
-print("Data Loaded:", data)  # Debug print
+# Convert JSON to DataFrame
+df = pd.DataFrame(data)
 
-# Convert to DataFrame
-df = pd.DataFrame(data[1:], columns=data[0])
+# Convert DataFrame to Markdown Table
+markdown_table = df.to_markdown()
 
-print("DataFrame Created:", df.head())  # Debug print
+# Read existing README
+with open('README.md', 'r') as file:
+    readme_content = file.read()
 
-# Convert to markdown table
-md_table = tabulate(df, headers='keys', tablefmt='pipe', showindex=False)
-
-print("Markdown Table:", md_table)  # Debug print
+# Find a position to insert the table or define a marker in README.md
+# For example, you can insert a comment <!-- DATA_TABLE --> in README.md where you want to insert the table
+start_index = readme_content.find("<!-- DATA_TABLE -->")
+if start_index != -1:
+    end_index = start_index + len("<!-- DATA_TABLE -->")
+    new_readme_content = (
+        readme_content[:end_index] + "\n\n" + markdown_table + "\n" + readme_content[end_index:]
+    )
+else:
+    new_readme_content = readme_content + "\n\n" + markdown_table
 
 # Update README.md
-with open('Library.md', 'w') as f:
-    f.write("# My Prompt Library\n\n")
-    f.write(md_table)
+with open('README.md', 'w') as file:
+    file.write(new_readme_content)
+
 
